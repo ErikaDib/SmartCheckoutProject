074ea2ccfbefd743e8879ef91fa56fa784dd6e05
Eduardo De La Cruz <eduardodelacruz2912@gmail.com>
2018-01-25 12:22:19 -0500

    added avility to populate prices and items modifying index.ejs and server.ejs

--- server.js
+++ server.js
@@ -11,33 +11,35 @@ var fs = require('fs');
 var storeInfo = fs.readFileSync('DummyData/walmart.json'); //return not a javaS object
 var infoarray = JSON.parse(storeInfo);
 
-
 // amazon items
 var amazonInfo = fs.readFileSync('DummyData/amazon.json'); //return not a javaS object
 var amazonArray = JSON.parse(amazonInfo);
 
 
 var coscoInfo = fs.readFileSync('DummyData/cosco.json'); //return not a javaS object
-var coscoArray = JSON.parse(amazonInfo);
-
-
-
-
-
+var coscoArray = JSON.parse(coscoInfo);
 
 
 
 // itemsArray added from the user form
 var itemsArray = [];
+
 //walmart Array of Item when to populate the walmart div
 var walmatItems = [];
+var walmartItemsValue = [];
+var walmartTotalPrice = 0;
+
+
 //amazon Array of Item when to populate the amazon div
-var amazonArray = [];
+var amazonItems = [];
+var amazontItemsValue = [];
+var amazonTotalPrice = 0;
 
 
 //cosco Array of Item when to populate the cosco div
-var coscoArray = [];
-
+var coscoItems = [];
+var coscoItemsValue = [];
+var coscoTotalPrice = 0;
 
 
 app.use(express.static('views'));
@@ -45,7 +47,11 @@ app.use(express.static('views'));
 app.use(express.static(__dirname + '/views'));
 
 app.get("/", function(req, res){
-    res.render("index.ejs",{listOfItems:itemsArray,Walmart:walmatItems,Amazon:amazonArray,Cosco:coscoArray});
+    res.render("index.ejs",
+    {listOfItems:itemsArray,
+        Walmart:walmatItems,WalmartPrices:walmartItemsValue,WalmartTotal:walmartTotalPrice,
+        Amazon:amazonItems,AmazonPrices:amazontItemsValue,AmazonTotal:amazonTotalPrice,
+        Cosco:coscoItems,CoscoPrices:coscoItemsValue,CoscoTotal:coscoTotalPrice });
 });
 // adding to the list
 app.post('/add',urlencodedParser,function(req, res)
@@ -83,7 +89,11 @@ app.get('/checkout',urlencodedParser,function(req, res)
          if(infoarray.hasOwnProperty(itemsArray[i]))
          {
             console.log("item found " + itemsArray[i]);
-            walmatItems.push(itemsArray[i]);            
+            walmatItems.push(itemsArray[i]);
+            walmartItemsValue.push(infoarray[itemsArray[i]]);
+            walmartTotalPrice = walmartTotalPrice + parseFloat(infoarray[itemsArray[i]]); 
+
+
          }
          else
          {
@@ -97,7 +107,9 @@ app.get('/checkout',urlencodedParser,function(req, res)
          if(infoarray.hasOwnProperty(itemsArray[i]))
          {
             console.log("item found " + itemsArray[i]);
-            amazonArray.push(itemsArray[i]);            
+            amazonItems.push(itemsArray[i]);
+            amazontItemsValue.push(amazonArray[itemsArray[i]]);
+            amazonTotalPrice = amazonTotalPrice + parseFloat(amazonArray[itemsArray[i]]);            
          }
          else
          {
@@ -111,16 +123,18 @@ app.get('/checkout',urlencodedParser,function(req, res)
          if(infoarray.hasOwnProperty(itemsArray[i]))
          {
             console.log("item found " + itemsArray[i]);
-            coscoArray.push(itemsArray[i]);            
+            coscoItems.push(itemsArray[i]);
+            coscoItemsValue.push(coscoArray[itemsArray[i]]);
+            coscoTotalPrice = coscoTotalPrice + parseFloat(coscoArray[itemsArray[i]]);
+
+
+            // console.log(coscoArray[itemsArray[i]]);         
          }
          else
          {
             console.log("object not found.Get foodstand");
          }
-
     }
-
-
     
     res.redirect('/');       
   
